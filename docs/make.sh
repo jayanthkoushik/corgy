a055c40d2729ca858b30097e798819cd4fe72ca6
Jayanth Koushik <jnkoushik@gmail.com>
2021-11-08 18:03:13 -0500

    docs: rename docs/index.md to docs/corgy.md and use new doc builder script

--- docs/make.sh
+++ docs/make.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
+# Usage: docs/make.sh  # must be executed from the project root
+# Generates markdown documentation for each top level package, except `tests`.
+# One file is generated per package, which is put in `docs/`.
 
-SPHINX_APIDOC_OPTIONS='members' poetry run sphinx-apidoc -o docs/_build -M -T corgy
-mv docs/_build/corgy.rst docs/_build/index.rst
-poetry run sphinx-build -D highlight_language=python -b markdown -c docs -d docs/_build/.doctrees docs/_build docs
-sed -i '' 's/ *$//' docs/index.md
+SPHINX_APIDOC_OPTIONS='members' \
+    poetry run sphinx-apidoc -o "docs/_build" -M -T . tests
+
+for f in docs/_build/*.rst ; do
+    rootdoc="$(basename "${f}" .rst)"
+    poetry run sphinx-build \
+        -D highlight_language=python \
+        -D root_doc="${rootdoc}" \
+        -b markdown \
+        -c "docs" \
+        -d "docs/_build/.doctrees" \
+        "docs/_build" "docs"
+
+    # Remove trailing whitespace.
+    sed -i '' 's/ *$//' "docs/${rootdoc}.md"
+done
