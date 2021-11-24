9ec99ce25794c62471af09876c2a6fabf6db9a2f
Jayanth Koushik <jnkoushik@gmail.com>
2021-11-23 22:59:30 -0500

    feat: show informative message if trying to import `Corgy` with Python < 3.9

--- corgy/__init__.py
+++ corgy/__init__.py
@@ -14,3 +14,16 @@ from ._helpfmt import *
 __all__ += _helpfmt.__all__  # type: ignore  # pylint: disable=undefined-variable
 
 from ._version import __version__
+
+_WARN_ABOUT_PY39 = True
+
+
+def __getattr__(name):
+    global _WARN_ABOUT_PY39  # pylint: disable=global-statement
+
+    if name == "Corgy" and _WARN_ABOUT_PY39:
+        import sys as _sys
+
+        print("ERROR: `Corgy` requires Python 3.9 or higher\n", file=_sys.stderr)
+        _WARN_ABOUT_PY39 = False
+    raise AttributeError
