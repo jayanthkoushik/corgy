e86e16c0aaa4def529c112e2db0f2ce2c29d7bfe
Jayanth Koushik <jnkoushik@gmail.com>
2021-12-01 21:08:44 -0500

    docs: generate separate doc file for submodules

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+# corgy
+
+
+* [corgy package](corgy.md)
+
+
+    * [Submodules](corgy.md#submodules)
+
+
+        * [corgy.types module](corgy.types.md)
--- docs/make.sh
+++ docs/make.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env sh
 # Usage: docs/make.sh  # must be executed from the project root
 # Generates markdown documentation for each top level package, except `tests`.
-# One file is generated per package, which is put in `docs/`.
+# One file is generated per module, which is put in `docs/`.
 
 SPHINX_APIDOC_OPTIONS='members' \
-    poetry run sphinx-apidoc -o "docs/_build" -M -T . tests
+    poetry run sphinx-apidoc -o "docs/_build" -M --tocfile index -e . tests
 
-for f in docs/_build/*.rst ; do
-    rootdoc="$(basename "${f}" .rst)"
-    poetry run sphinx-build \
-        -D highlight_language=python \
-        -D root_doc="${rootdoc}" \
-        -b markdown \
-        -c "docs" \
-        -d "docs/_build/.doctrees" \
-        "docs/_build" "docs"
+poetry run sphinx-build \
+    -D highlight_language=python \
+    -b markdown \
+    -c "docs" \
+    -d "docs/_build/.doctrees" \
+    "docs/_build" "docs" `ls docs/_build/*.rst`
 
-    # Remove trailing whitespace.
-    sed -i '' 's/ *$//' "docs/${rootdoc}.md"
-done
+find docs -name '*.md' -exec sed -i '' 's/ *$//' {} +
